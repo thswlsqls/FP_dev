3d4bfbd4b18cf1221eea36c229c1beb2a2b43511
thswlsqls <thswlsqls@naver.com>
2024-02-03 21:30:31 +0900

    Template class updated

--- src/main/java/com/ebson/skillserver/common/Template.java
+++ src/main/java/com/ebson/skillserver/common/Template.java
@@ -8,7 +8,7 @@ import java.util.Map;
 @Data
 public class Template {
     // template 내부 구조에 따라 필드 추가
-    private List<Object> outputs; // Object 는 Component에 해당하고 SimpleText, SimpleImage, TextCard, BasicCard, CommerceCard, ListCard, ItemCard, Carousel 클래스가 될 수 있음.
+    private List<Map<String, Object>> outputs; // Map<String, Object> 는 Component에 해당하고 SimpleText, SimpleImage, TextCard, BasicCard, CommerceCard, ListCard, ItemCard, Carousel 클래스가 Object 값 으로 사용될 수 있음.
     private List<QuickReply> quickReplies;
 
     @Data
