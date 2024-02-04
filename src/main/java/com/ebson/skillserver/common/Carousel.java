470d08fb74a2abd3d78c4e5c39eaec0e134c8bf4
thswlsqls <thswlsqls@naver.com>
2024-02-04 17:12:14 +0900

    Carousel example added

--- src/main/java/com/ebson/skillserver/common/Carousel.java
+++ src/main/java/com/ebson/skillserver/common/Carousel.java
@@ -3,11 +3,12 @@ package com.ebson.skillserver.common;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class Carousel {
     private String type;
-    private List<Object> items;
+    private List<Map<String, Object>> items;
     private CarouselHeader header;
 
     @Data
