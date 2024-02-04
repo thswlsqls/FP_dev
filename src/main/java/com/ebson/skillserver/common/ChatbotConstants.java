bc03454d76beebc752e7f71429ffc5c09568e455
thswlsqls <thswlsqls@naver.com>
2024-02-04 17:11:47 +0900

    ChatbotConstants updated

--- src/main/java/com/ebson/skillserver/common/ChatbotConstants.java
+++ src/main/java/com/ebson/skillserver/common/ChatbotConstants.java
@@ -5,14 +5,14 @@ public final class ChatbotConstants {
     public static final String VERSION = "2.0";
 
     // 컴포넌트 타입 상수
-    public static final String SIMPLE_TEXT = "SimpleText";
-    public static final String SIMPLE_IMAGE = "SimpleImage";
-    public static final String TEXT_CARD = "TextCard";
-    public static final String BASIC_CARD = "BasicCard";
-    public static final String COMMERCE_CARD = "CommerceCard";
-    public static final String LIST_CARD = "ListCard";
-    public static final String ITEM_CARD = "ItemCard";
-    public static final String CAROUSEL = "Carousel";
+    public static final String SIMPLE_TEXT = "simpleText";
+    public static final String SIMPLE_IMAGE = "simpleImage";
+    public static final String TEXT_CARD = "textCard";
+    public static final String BASIC_CARD = "basicCard";
+    public static final String COMMERCE_CARD = "commerceCard";
+    public static final String LIST_CARD = "listCard";
+    public static final String ITEM_CARD = "itemCard";
+    public static final String CAROUSEL = "carousel";
 
     // 기타 고정 문자열 상수
     public static final String WELCOME_MESSAGE = "Welcome to our Chatbot";
