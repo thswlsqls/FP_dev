5acadb2b73963224489e5f28d8fefeefdd5de978
thswlsqls <thswlsqls@naver.com>
2024-02-04 14:06:25 +0900

    comment added

--- src/main/java/com/ebson/skillserver/common/Profile.java
+++ src/main/java/com/ebson/skillserver/common/Profile.java
@@ -4,6 +4,6 @@ import lombok.Data;
 
 @Data
 public class Profile {
-    private String nickname;
-    private String imageUrl;
+    private String nickname; // 필수O 프로필 이름
+    private String imageUrl; // 필수X 프로필 이미지
 }
