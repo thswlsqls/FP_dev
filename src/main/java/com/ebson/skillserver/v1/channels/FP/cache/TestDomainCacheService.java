9fb09dca33bfac8fc9865c9ca193c610c4927a4a
thswlsqls <thswlsqls@naver.com>
2024-02-06 23:39:30 +0900

    TestDomainCacheService added

--- src/main/java/com/ebson/skillserver/v1/channels/FP/cache/TestDomainCacheService.java
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/cache/TestDomainCacheService.java
@@ -23,10 +23,20 @@ public class TestDomainCacheService {
         return null; // 캐시 미스 시 DB 조회 코드 생략
     }
 
+    @Cacheable(value = "TestDomains", key = "'TestDomain:' + #id + ':' + #name", condition = "#no > 10", unless = "#no == null", sync = true)
+    public TestDomain getTestDomainCustom(String id, String name) {
+        // condition: 캐시를 적용할 조건을 지정할 수 있습니다. 이 속성은 SpEL을 사용하여 평가되며, 조건이 true일 때만 캐싱이 적용됩니다.
+        // unless: 메서드의 반환값을 캐싱하지 않을 조건을 지정합니다. 이 속성 역시 SpEL을 사용하여 평가되며, 조건이 true일 때 결과가 캐시에 저장되지 않습니다.
+        // sync: 캐시 미스가 발생했을 때 여러 스레드가 동시에 같은 메서드를 호출하는 상황을 방지합니다.
+        // sync = true로 설정하면 한 번에 하나의 스레드만 메서드를 실행하고 결과를 캐시에 저장할 수 있습니다. 이는 캐시 스톰(cache storm)을 방지하는 데 유용합니다.
+        return null; // 캐시 미스 시 DB 조회 코드 생략
+    }
+
     @Data
     public class TestDomain {
         private String id;
         private String name;
+        private Integer no;
     }
 
 }
