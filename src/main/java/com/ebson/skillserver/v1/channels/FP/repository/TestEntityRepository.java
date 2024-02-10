8ffcb979b044225a73631b62fc0b94a356c5cb2b
thswlsqls <thswlsqls@naver.com>
2024-02-10 13:42:17 +0900

    TestEntityRepository updated

--- src/main/java/com/ebson/skillserver/v1/channels/FP/repository/TestEntityRepository.java
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/repository/TestEntityRepository.java
@@ -2,14 +2,20 @@ package com.ebson.skillserver.v1.channels.FP.repository;
 
 import com.ebson.skillserver.v1.channels.FP.entity.TestEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 public interface TestEntityRepository extends JpaRepository<TestEntity, UUID> {
 
-    /** 조회 */
+    /** 1. JpaRepository 기본 제공 문법 */
+    /** 1.1. 조회 */
     // 이름으로 데이터 찾기
     List<TestEntity> findByTestName(String testName);
 
@@ -41,13 +47,50 @@ public interface TestEntityRepository extends JpaRepository<TestEntity, UUID> {
     List<TestEntity> findByTestNameIn(List<String> testNames);
 
 
-    /** 수 세기 */
+    /** 1.2. 수 세기 */
     // 특정 이름을 가진 데이터의 수 세기
     long countByTestName(String testName);
 
 
-    /** 삭제 */
+    /** 1.3. 삭제 */
     // 특정 이름을 가진 데이터 삭제
     long deleteByTestName(String testName);
+
+    /** 2. JPQL */
+    /** 2.1. 조회 */
+    // 이름으로 데이터 조회
+    @Query("SELECT u FROM TestEntity tu WHERE tu.testName = :testName")
+    List<TestEntity> findByTestNameWithJPQL(@Param("testName") String testName);
+
+    // 플래그 값으로 데이터 조회
+    @Query("SELECT tu FROM TestEntity tu WHERE tu.testFlag = true")
+    List<TestEntity> findActiveUsersWithJPQL();
+
+    // 특정 숫자 이상의 데이터 조회
+    @Query("SELECT tu FROM TestEntity tu WHERE tu.testNo >= :testNo")
+    List<TestEntity> findTestEntitiesGreaterThanWithJPQL(@Param("testNo") int testNo);
+
+    /** 2.2. 수정 */
+    @Transactional
+    @Modifying
+    @Query("UPDATE TestEntity tu SET t.testFlag = false WHERE tu.testNo < :testNo")
+    int deactivateYoungUsers(@Param("testNo") int testNo);
+
+    /** 3. Native Query */
+    /** 3.1. 조회 */
+    // 네이티브 SQL을 사용하여 이름으로 데이터 조회
+    @Query(value = "SELECT * FROM TestEntity WHERE testName = :testName", nativeQuery = true)
+    List<TestEntity> findByTestNameNative(@Param("testName") String testName);
+
+    // 네이티브 SQL을 사용하여 플래그 값으로 데이터의 수 조회
+    @Query(value = "SELECT COUNT(*) FROM test_entity WHERE test_flag = 1", nativeQuery = true)
+    int countTestFlagTrueTestEntitiesNative();
+
+    // 이름과 이메일로 데이터 조회
+    @Query(value = "SELECT * FROM test_entity WHERE test_name = :testName AND test_email = :testEmail", nativeQuery = true)
+    List<TestEntity> findByTestNameAndTestEmailNative(@Param("testName") String testName, @Param("testEmail") String testEmail);
+
+    @Query(value = "SELECT * FROM users WHERE created_date >= :startDate AND created_date <= :endDate", nativeQuery = true)
+    List<TestEntity> findByCreatedDateBetween(@Param("startDate") Date startDate, @Param("endDate") Date endDate);
 
 }
