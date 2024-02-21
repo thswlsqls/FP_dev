81f0f6d872868b84f120992d82a69dd97e35e333
thswlsqls <thswlsqls@naver.com>
2024-02-22 00:03:54 +0900

    builder domains added

--- src/main/java/com/ebson/skillserver/v1/channels/FP/domain/BuilderV1BlockContextDomain.java
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/domain/BuilderV1BlockContextDomain.java
@@ -1,18 +1,18 @@
 package com.ebson.skillserver.v1.channels.FP.domain;
 
-
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-public class BuilderV1ScenarioDomain {
-    private UUID scenarioId;
-    private UUID channelId;
-    private String scenarioName;
+public class BuilderV1BlockContextDomain {
+    private UUID contextId;
+    private UUID blockId;
+    private String contextType;
+    private String contextName;
     private LocalDateTime createdDate;
-    private LocalDateTime lastUpdatedDate;
+    private LocalDateTime lastUpdateDate;
     private UUID creator;
     private UUID lastUpdater;
 }
--- src/main/java/com/ebson/skillserver/v1/channels/FP/domain/BuilderV1BlockDomain.java
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/domain/BuilderV1BlockDomain.java
@@ -1,16 +1,15 @@
 package com.ebson.skillserver.v1.channels.FP.domain;
 
-
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-public class BuilderV1ScenarioDomain {
+public class BuilderV1BlockDomain {
+    private UUID blockId;
     private UUID scenarioId;
-    private UUID channelId;
-    private String scenarioName;
+    private String blockName;
     private LocalDateTime createdDate;
     private LocalDateTime lastUpdatedDate;
     private UUID creator;
