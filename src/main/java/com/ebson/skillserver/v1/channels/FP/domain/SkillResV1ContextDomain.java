aa61ae1a8352ad92cdeca38e2ec05b537b2ff18b
thswlsqls <thswlsqls@naver.com>
2024-02-23 00:18:12 +0900

    SkillResV1ContextDomain added

---
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/domain/SkillResV1ContextDomain.java
@@ -0,0 +1,20 @@
+package com.ebson.skillserver.v1.channels.FP.domain;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class SkillResV1ContextDomain {
+    private UUID contextId;
+    private UUID blockId;
+    private String contextName;
+    private String contextType;
+    private Integer lifespan;
+    private Integer ttl;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastUpdatedDate;
+    private UUID creator;
+    private UUID lastUpdater;
+}
