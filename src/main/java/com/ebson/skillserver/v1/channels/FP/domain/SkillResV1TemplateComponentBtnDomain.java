e556e4fef31cd03bf5560d7c40bffb7d564f93d3
thswlsqls <thswlsqls@naver.com>
2024-02-24 23:30:22 +0900

    SkillResV1TemplateComponentBtnDomain added

--- src/main/java/com/ebson/skillserver/v1/channels/FP/domain/SkillResV1TemplateComponentBtnDomain.java
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/domain/SkillResV1TemplateComponentBtnDomain.java
@@ -1,2 +1,22 @@
-package com.ebson.skillserver.v1.channels.FP.domain;public class SkillResV1TemplateComponentBtnDomain {
+package com.ebson.skillserver.v1.channels.FP.domain;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class SkillResV1TemplateComponentBtnDomain {
+    private UUID btnId;
+    private UUID componentId;
+    private String label;
+    private String action;
+    private String webLinkUrl;
+    private String messageText;
+    private String phone;
+    private UUID blockId; // Optional, may be null if not linked to a block
+    private LocalDateTime createdDate;
+    private LocalDateTime lastUpdatedDate;
+    private UUID creator;
+    private UUID lastUpdater;
 }
