e579d4fb8069ef251ab6e7f63adb3c03b4d47168
thswlsqls <thswlsqls@naver.com>
2024-02-24 23:38:25 +0900

    SkillResV1TemplateComponentBtnExtraDomain added

---
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/domain/SkillResV1TemplateComponentBtnExtraDomain.java
@@ -0,0 +1,18 @@
+package com.ebson.skillserver.v1.channels.FP.domain;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class SkillResV1TemplateComponentBtnExtraDomain {
+    private UUID extraId;
+    private UUID btnId;
+    private String key;
+    private String value;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastUpdatedDate;
+    private UUID creator;
+    private UUID lastUpdater;
+}
