ccd181d35ec96ec11b3c975e5f4a8f81ed8d07e9
thswlsqls <thswlsqls@naver.com>
2024-02-24 23:19:51 +0900

    SkillResV1TemplateComponentDomain added

---
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/domain/SkillResV1TemplateComponentDomain.java
@@ -0,0 +1,19 @@
+package com.ebson.skillserver.v1.channels.FP.domain;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+public class SkillResV1TemplateComponentDomain {
+    private UUID componentId;
+    private UUID outputId;
+    private String componentType;
+    private String componentName;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastUpdatedDate;
+    private UUID creator;
+    private UUID lastUpdater;
+
+}
