c1a5ccf34d8c82db8550e111b73b71fd0e6d69af
thswlsqls <thswlsqls@naver.com>
2024-03-03 19:29:30 +0900

    BuilderV1BlockDomainMapper added

---
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/mapper/BuilderV1BlockDomainMapper.java
@@ -0,0 +1,17 @@
+package com.ebson.skillserver.v1.channels.FP.mapper;
+
+import com.ebson.skillserver.v1.channels.FP.domain.BuilderV1BlockDomain;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.UUID;
+
+@Mapper
+public interface BuilderV1BlockDomainMapper {
+    BuilderV1BlockDomain selectByBlockId(UUID blockId);
+    List<BuilderV1BlockDomain> selectAll();
+    void insertBuilderV1Block(BuilderV1BlockDomain builderV1BlockDomain);
+    void updateBuilderV1Block(BuilderV1BlockDomain builderV1BlockDomain);
+    void deleteByBlockId(UUID blockId);
+}
+
