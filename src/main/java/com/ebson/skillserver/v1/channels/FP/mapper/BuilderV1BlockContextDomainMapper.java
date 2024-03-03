abeb6ccdf3e8dfd28de37ce3f31cdd57f76c39f6
thswlsqls <thswlsqls@naver.com>
2024-03-03 20:47:15 +0900

    BuilderV1BlockContextDomainMapper addded

---
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/mapper/BuilderV1BlockContextDomainMapper.java
@@ -0,0 +1,16 @@
+package com.ebson.skillserver.v1.channels.FP.mapper;
+
+import com.ebson.skillserver.v1.channels.FP.domain.BuilderV1BlockContextDomain;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.UUID;
+
+@Mapper
+public interface BuilderV1BlockContextDomainMapper {
+    BuilderV1BlockContextDomain selectByContextId(UUID contextId);
+    List<BuilderV1BlockContextDomain> selectAll();
+    void insertBuilderV1BlockContext(BuilderV1BlockContextDomain builderV1BlockContextDomain);
+    void updateBuilderV1BlockContext(BuilderV1BlockContextDomain builderV1BlockContextDomain);
+    void deleteByContextId(UUID contextId);
+}
