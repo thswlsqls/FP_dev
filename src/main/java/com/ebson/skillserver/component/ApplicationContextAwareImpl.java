1d03634b413d71dae98e7ae58e60afbbe2e50762
thswlsqls <thswlsqls@naver.com>
2024-03-09 21:37:32 +0900

    ApplicationContextAwareImpl added

---
+++ src/main/java/com/ebson/skillserver/component/ApplicationContextAwareImpl.java
@@ -0,0 +1,23 @@
+package com.ebson.skillserver.component;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextAwareImpl implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextAwareImpl.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+}
