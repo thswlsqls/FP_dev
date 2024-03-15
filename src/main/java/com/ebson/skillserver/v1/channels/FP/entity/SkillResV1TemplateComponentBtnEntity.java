56ef5f96b282cdd7f013f42ec0578d799f42bd4d
thswlsqls <thswlsqls@naver.com>
2024-03-15 14:08:05 +0900

    commit changes

---
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/entity/SkillResV1TemplateComponentBtnEntity.java
@@ -0,0 +1,70 @@
+package com.ebson.skillserver.v1.channels.FP.entity;
+
+import com.ebson.skillserver.converter.UUIDToBytesConverter;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "SKILL_RES_V1_TEMPLATE_COMPONENT_BTN")
+@Getter @Setter
+public class SkillResV1TemplateComponentBtnEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "BTN_ID", nullable = false, columnDefinition = "BINARY(16)")
+    @Convert(converter = UUIDToBytesConverter.class)
+    private UUID btnId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "COMPONENT_ID", referencedColumnName = "COMPONENT_ID", nullable = false, foreignKey = @ForeignKey(name = "FK_BTN_COMPONENT_ID"))
+    private SkillResV1TemplateComponentEntity skillResV1TemplateComponentEntity;
+
+    @Column(name = "LABEL", length = 45)
+    private String label;
+
+    @Column(name = "ACTION", length = 45)
+    private String action;
+
+    @Column(name = "WEB_LINK_URL", length = 255)
+    private String webLinkUrl;
+
+    @Column(name = "MESSAGE_TEXT", length = 255)
+    private String messageText;
+
+    @Column(name = "PHONE", length = 45)
+    private String phone;
+
+    @Column(name = "BLOCK_ID", length = 45)
+    private String blockId;
+
+    @Column(name = "CREATED_DATE", nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "LAST_UPDATED_DATE", nullable = false)
+    private LocalDateTime lastUpdatedDate;
+
+    @Column(name = "CREATOR", length = 45)
+    private String creator;
+
+    @Column(name = "LAST_UPDATER", length = 45)
+    private String lastUpdater;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdDate = now;
+        this.lastUpdatedDate = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.lastUpdatedDate = LocalDateTime.now();
+    }
+}
+
---
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/repository/SkillResV1TemplateComponentBtnEntityRepository.java
@@ -0,0 +1,11 @@
+package com.ebson.skillserver.v1.channels.FP.repository;
+
+import com.ebson.skillserver.v1.channels.FP.entity.SkillResV1TemplateComponentBtnEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface SkillResV1TemplateComponentBtnEntityRepository extends JpaRepository<SkillResV1TemplateComponentBtnEntity, UUID> {
+}
