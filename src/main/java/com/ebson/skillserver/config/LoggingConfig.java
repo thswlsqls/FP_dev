859bff4ddc03f071f572d7cadd978dd8b423ad7d
thswlsqls <thswlsqls@naver.com>
2024-05-05 15:42:25 +0900

    LoggingConfig added

--- src/main/java/com/ebson/skillserver/config/LoggingConfig.java
+++ src/main/java/com/ebson/skillserver/config/LoggingConfig.java
@@ -52,6 +52,10 @@ public class LoggingConfig {
     public static class RollingFileLoggingConfiguration {
         public RollingFileLoggingConfiguration() {
             configureErrorFileLogging();
+            configureTraceFileLogging();
+            configureDebugFileLogging();
+            configureInfoFileLogging();
+            configureWarnFileLogging();
         }
 
         private void configureErrorFileLogging() {
@@ -78,6 +82,107 @@ public class LoggingConfig {
             // context.getLogger("ERROR_LOGGER").setLevel(ch.qos.logback.classic.Level.DEBUG);
             context.getLogger("ERROR_LOGGER").addAppender(rollingFileAppender);
         }
+
+        private void configureTraceFileLogging() {
+            LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+            RollingFileAppender<ILoggingEvent> rollingFileAppender = new RollingFileAppender<>();
+            rollingFileAppender.setContext(context);
+            rollingFileAppender.setName("TRACE_LOGGER");
+            rollingFileAppender.setFile("logs/trace.log");
+
+            LevelFilter levelFilter = new LevelFilter();
+            levelFilter.setLevel(Level.TRACE);
+            levelFilter.setOnMatch(FilterReply.ACCEPT);
+            levelFilter.setOnMismatch(FilterReply.DENY);
+
+            SizeAndTimeBasedRollingPolicy rollingPolicy = new SizeAndTimeBasedRollingPolicy();
+            rollingPolicy.setContext(context);
+            rollingPolicy.setFileNamePattern("logs/trace/trace.%d{yyyy-MM-dd}_%i.log");
+            rollingPolicy.setMaxFileSize(FileSize.valueOf("20MB"));
+            rollingPolicy.setMaxHistory(30);
+            rollingPolicy.setParent(rollingFileAppender);
+            rollingPolicy.start();
+
+            // context.getLogger("ERROR_LOGGER").setLevel(ch.qos.logback.classic.Level.DEBUG);
+            context.getLogger("TRACE_LOGGER").addAppender(rollingFileAppender);
+        }
+
+        private void configureDebugFileLogging() {
+            LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+            RollingFileAppender<ILoggingEvent> rollingFileAppender = new RollingFileAppender<>();
+            rollingFileAppender.setContext(context);
+            rollingFileAppender.setName("DEBUG_LOGGER");
+            rollingFileAppender.setFile("logs/debug.log");
+
+            LevelFilter levelFilter = new LevelFilter();
+            levelFilter.setLevel(Level.DEBUG);
+            levelFilter.setOnMatch(FilterReply.ACCEPT);
+            levelFilter.setOnMismatch(FilterReply.DENY);
+
+            SizeAndTimeBasedRollingPolicy rollingPolicy = new SizeAndTimeBasedRollingPolicy();
+            rollingPolicy.setContext(context);
+            rollingPolicy.setFileNamePattern("logs/debug/debug.%d{yyyy-MM-dd}_%i.log");
+            rollingPolicy.setMaxFileSize(FileSize.valueOf("20MB"));
+            rollingPolicy.setMaxHistory(30);
+            rollingPolicy.setParent(rollingFileAppender);
+            rollingPolicy.start();
+
+            // context.getLogger("ERROR_LOGGER").setLevel(ch.qos.logback.classic.Level.DEBUG);
+            context.getLogger("DEBUG_LOGGER").addAppender(rollingFileAppender);
+        }
+
+        private void configureInfoFileLogging() {
+            LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+            RollingFileAppender<ILoggingEvent> rollingFileAppender = new RollingFileAppender<>();
+            rollingFileAppender.setContext(context);
+            rollingFileAppender.setName("INFO_LOGGER");
+            rollingFileAppender.setFile("logs/info.log");
+
+            LevelFilter levelFilter = new LevelFilter();
+            levelFilter.setLevel(Level.INFO);
+            levelFilter.setOnMatch(FilterReply.ACCEPT);
+            levelFilter.setOnMismatch(FilterReply.DENY);
+
+            SizeAndTimeBasedRollingPolicy rollingPolicy = new SizeAndTimeBasedRollingPolicy();
+            rollingPolicy.setContext(context);
+            rollingPolicy.setFileNamePattern("logs/info/info.%d{yyyy-MM-dd}_%i.log");
+            rollingPolicy.setMaxFileSize(FileSize.valueOf("20MB"));
+            rollingPolicy.setMaxHistory(30);
+            rollingPolicy.setParent(rollingFileAppender);
+            rollingPolicy.start();
+
+            // context.getLogger("ERROR_LOGGER").setLevel(ch.qos.logback.classic.Level.DEBUG);
+            context.getLogger("INFO_LOGGER").addAppender(rollingFileAppender);
+        }
+
+        private void configureWarnFileLogging() {
+            LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+            RollingFileAppender<ILoggingEvent> rollingFileAppender = new RollingFileAppender<>();
+            rollingFileAppender.setContext(context);
+            rollingFileAppender.setName("WARN_LOGGER");
+            rollingFileAppender.setFile("logs/warn.log");
+
+            LevelFilter levelFilter = new LevelFilter();
+            levelFilter.setLevel(Level.WARN);
+            levelFilter.setOnMatch(FilterReply.ACCEPT);
+            levelFilter.setOnMismatch(FilterReply.DENY);
+
+            SizeAndTimeBasedRollingPolicy rollingPolicy = new SizeAndTimeBasedRollingPolicy();
+            rollingPolicy.setContext(context);
+            rollingPolicy.setFileNamePattern("logs/warn/warn.%d{yyyy-MM-dd}_%i.log");
+            rollingPolicy.setMaxFileSize(FileSize.valueOf("20MB"));
+            rollingPolicy.setMaxHistory(30);
+            rollingPolicy.setParent(rollingFileAppender);
+            rollingPolicy.start();
+
+            // context.getLogger("ERROR_LOGGER").setLevel(ch.qos.logback.classic.Level.DEBUG);
+            context.getLogger("WARN_LOGGER").addAppender(rollingFileAppender);
+        }
+
     }
 
 }
