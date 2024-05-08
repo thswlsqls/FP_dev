79571369bf0714aa1b12e1d2933db5de0e5fe295
thswlsqls <thswlsqls@naver.com>
2024-05-08 19:11:34 +0900

    SkillResponseCacheService added

--- src/main/java/com/ebson/skillserver/cache/skillResponse/SkillResponseCacheService.java
+++ src/main/java/com/ebson/skillserver/cache/skillResponse/SkillResponseCacheService.java
@@ -1,5 +1,6 @@
 package com.ebson.skillserver.cache.skillResponse;
 
+import com.ebson.skillserver.util.UUIDFormatter;
 import com.ebson.skillserver.v1.channels.FP.entity.SkillResV1ContextEntity;
 import com.ebson.skillserver.v1.channels.FP.entity.SkillResV1ContextParamEntity;
 import com.ebson.skillserver.v1.channels.FP.entity.SkillResV1DataEntity;
@@ -51,12 +52,21 @@ public class SkillResponseCacheService {
     @EventListener(value= ApplicationReadyEvent.class)
     public void init() {
         List<SkillResV1ContextEntity> list1 = skillResV1ContextEntityRepository.findAll();
-
+        for (SkillResV1ContextEntity entity : list1) {
+            skillResV1ContextCacheService.setSkillResV1ContextDomainCache(UUIDFormatter.formatToUUID(entity.getContextId().toString()));
+        }
         List<SkillResV1ContextParamEntity> list2 = skillResV1ContextParamEntityRepository.findAll();
-
+        for (SkillResV1ContextParamEntity entity : list2) {
+            skillResV1ContextParamCacheService.setSkillResV1ContextParamDomainCache(UUIDFormatter.formatToUUID(entity.getContextId().toString()), entity.getParamNo());
+        }
         List<SkillResV1DataEntity> list3 = skillResV1DataEntityRepository.findAll();
-
+        for (SkillResV1DataEntity entity : list3) {
+            skillResV1DataCacheService.setSkillResV1DataDomainCache(UUIDFormatter.formatToUUID(entity.getDataId().toString()));
+        }
         List<SkillResV1VersionEntity> list4 = skillResV1VersionEntityRepository.findAll();
+        for (SkillResV1VersionEntity entity : list4) {
+            skillResV1VersionCacheService.setSkillResV1VersionDomainCache(UUIDFormatter.formatToUUID(entity.getVersionId().toString()));
+        }
     }
 }
 
