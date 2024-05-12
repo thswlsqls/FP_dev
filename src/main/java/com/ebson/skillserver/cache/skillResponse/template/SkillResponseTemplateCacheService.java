dcdbc321425fa3846ad4adfbdaa47562b4a8ecbc
thswlsqls <thswlsqls@naver.com>
2024-05-12 17:17:56 +0900

    commit changes

--- src/main/java/com/ebson/skillserver/cache/skillResponse/template/SkillResponseTemplateCacheService.java
+++ src/main/java/com/ebson/skillserver/cache/skillResponse/template/SkillResponseTemplateCacheService.java
@@ -1,5 +1,10 @@
 package com.ebson.skillserver.cache.skillResponse.template;
 
+import com.ebson.skillserver.util.UUIDFormatter;
+import com.ebson.skillserver.v1.channels.FP.entity.SkillResV1TemplateCarouselEntity;
+import com.ebson.skillserver.v1.channels.FP.entity.SkillResV1TemplateComponentEntity;
+import com.ebson.skillserver.v1.channels.FP.entity.SkillResV1TemplateEntity;
+import com.ebson.skillserver.v1.channels.FP.entity.SkillResV1TemplateOutputEntity;
 import com.ebson.skillserver.v1.channels.FP.repository.SkillResV1TemplateCarouselEntityRepository;
 import com.ebson.skillserver.v1.channels.FP.repository.SkillResV1TemplateComponentEntityRepository;
 import com.ebson.skillserver.v1.channels.FP.repository.SkillResV1TemplateEntityRepository;
@@ -11,6 +16,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class SkillResponseTemplateCacheService {
@@ -46,9 +53,22 @@ public class SkillResponseTemplateCacheService {
     @EventListener(value= ApplicationReadyEvent.class)
     public void init() {
         logger.info("SkillResV1TemplateCacheService initialized ... ");
-
-
-
+        List<SkillResV1TemplateEntity> list1 = skillResV1TemplateEntityRepository.findAll();
+        for (SkillResV1TemplateEntity entity : list1) {
+            skillResV1TemplateCacheService.setSkillResV1TemplateDomainCache(UUIDFormatter.formatToUUID(entity.getTemplateId().toString()));
+        }
+        List<SkillResV1TemplateOutputEntity> list2 = skillResV1TemplateOutputEntityRepository.findAll();
+        for (SkillResV1TemplateOutputEntity entity : list2) {
+            skillResV1TemplateOutputCacheService.setSkillResV1TemplateOutputDomainCache(UUIDFormatter.formatToUUID(entity.getOutputId().toString()));
+        }
+        List<SkillResV1TemplateComponentEntity> list3 = skillResV1TemplateComponentEntityRepository.findAll();
+        for (SkillResV1TemplateComponentEntity entity : list3) {
+            skillResV1TemplateComponentCacheService.setSkillResV1TemplateComponentDomainCache(UUIDFormatter.formatToUUID(entity.getComponentId().toString()));
+        }
+        List<SkillResV1TemplateCarouselEntity> list4 = skillResV1TemplateCarouselEntityRepository.findAll();
+        for (SkillResV1TemplateCarouselEntity entity : list4) {
+            skillResV1TemplateCarouselCacheService.setSkillResV1TemplateCarouselDomainCache(UUIDFormatter.formatToUUID(entity.getCarouselId().toString()));
+        }
     }
 
 
