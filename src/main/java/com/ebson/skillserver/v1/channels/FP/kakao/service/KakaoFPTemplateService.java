b922d4bbf81890fb7dbb4e10b03bff1ace663f4f
thswlsqls <thswlsqls@naver.com>
2024-07-02 21:15:15 +0900

    commit changes

--- src/main/java/com/ebson/skillserver/v1/channels/FP/kakao/service/KakaoFPTemplateService.java
+++ src/main/java/com/ebson/skillserver/v1/channels/FP/kakao/service/KakaoFPTemplateService.java
@@ -1146,7 +1146,13 @@ public class KakaoFPTemplateService {
         try {
             log.info("KakaoFPTemplateService^^getCarouselItemCard() :: output : {}", om.writeValueAsString(output));
         } catch (JsonProcessingException e){
-            e.printStackTrace();
+            StackTraceElement[] stet = e.getStackTrace();
+            String className = stet[0].getClassName();
+            String metodName = stet[0].getMethodName();
+            int lineNum = stet[0].getLineNumber();
+            String fileNeme = stet[0].getFileName();
+            log.error("Exception : {}, className : {} , methodName : {}, fileName : {}, lineNum : {}",
+                    e.getMessage(), className, metodName, fileNeme, lineNum);
         }
         return output;
     }
